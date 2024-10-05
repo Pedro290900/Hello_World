2c8989266715d4400f5181ed2a1860cac2c31159
Pedro290900 <pcoutinhoduarte@gmail.com>
2024-10-04 21:49:03 -0300

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+integer contagem =0;
+
+ArrayList<Integer> numero = new ArrayList<Integer>();
+
+int[] matriculas = {10,20,18,27,26};
+
+for quantidade in matriculas:
+  contagem=contagem+1;
+
+System.out.println("A quantidade de estudantes é"+contagem);
+  
